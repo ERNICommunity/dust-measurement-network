2f5fbef85e193ca851fcd253e5374d2dcc989aee
Ján Neščivera <jan.nescivera@erni.sk>
2018-08-21 10:45:12 +0200

    use default configuration

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 export function getBaseUrl() {
-  return 'http://localhost:5000/';
+  return document.getElementsByTagName('base')[0].href;
 }
 
 const providers = [
