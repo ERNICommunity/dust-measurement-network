4a04a642447c87575b16efc051cfed197fbc89db
Michel Meyer <meyer_michel@msn.com>
2018-08-25 19:50:15 +0200

    implement get data type from union

--- ttn-gwy/TTN_Interface.go
+++ ttn-gwy/TTN_Interface.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strconv"
 
 	"./protobuf"
 	"github.com/TheThingsNetwork/go-utils/log"
@@ -27,6 +28,20 @@ func checkError(err error) {
 	}
 }
 
+func decodeBatteryState(devId string,
+	latitude float32,
+	longitude float32,
+	altitude int32,
+	time types.JSONTime,
+	data *protobuf.BatteryState) {
+	voltage := data.Voltage
+	fmt.Println("sensor_state,sensor_id=" + devId + " voltage=" + strconv.FormatFloat(float64(*voltage), 'f', 6, 32) + ",health_state=" + strconv.FormatFloat(float64(*data.State), 'f', 1, 32) + " ")
+}
+
+func decodeDustMeasurement() {
+
+}
+
 func decodeNodeState(appID string, devID string, req types.UplinkMessage) {
 	length := binary.Size(req.PayloadRaw)
 	fmt.Println("Decoding NodeStatus Protobuf message")
@@ -34,6 +49,8 @@ func decodeNodeState(appID string, devID string, req types.UplinkMessage) {
 	protodata := new(protobuf.NodeMessage)
 	//Convert all the data retrieved into the ProtobufTest.TestMessage struct type
 	err := proto.Unmarshal(req.PayloadRaw[0:length], protodata)
+	fmt.Println(protodata.Information)
+
 	if err == nil {
 		fmt.Println("DEVICE ID ")
 		fmt.Println(devID)
@@ -49,6 +66,21 @@ func decodeNodeState(appID string, devID string, req types.UplinkMessage) {
 		fmt.Printf("%s", data)
 		fmt.Println("")
 		fmt.Println("Data Node State: ")
+		fmt.Println()
+		/*amountOfString, err := f.WriteString("writes\n")
+
+		f.Sync()*/
+		switch *protodata.Information {
+		case protobuf.NodeMessage_BATTERY_STATE:
+			switch u := protodata.Msg.(type) {
+			case *protobuf.NodeMessage_BatteryStateData:
+				decodeBatteryState(devID, req.Metadata.Latitude, req.Metadata.Longitude, req.Metadata.Altitude, req.Metadata.Time, u.BatteryStateData)
+			}
+
+		case protobuf.NodeMessage_DUST_MEASUREMENT:
+			fmt.Println("BATTERY STATE")
+		}
+
 		fmt.Println(protodata)
 	}
 }
@@ -61,7 +93,10 @@ func main() {
 	if err := client.Connect(); err != nil {
 		ctx.WithError(err).Fatal("Could not connect")
 	}
-	fmt.Println("HELLO WORLD")
+
+	/*f, err := os.Create("output.txt")
+	check(err)*/
+	//defer f.Close()
 
 	token := client.SubscribeAppUplink("erni-hello-world", func(client mqtt.Client, appID string, devID string, req types.UplinkMessage) {
 		fmt.Println("------Received - UPLINK ------")
