0f62f2199774d7f920b15bf8cd80b4f6522ccc84
Marek Linka <marek.linka@outlook.com>
2018-08-31 11:32:54 +0200

    Fix training data preparation

--- MachineLearning/data/train_model.py
+++ MachineLearning/data/train_model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import scale, OneHotEncoder
+import pickle
 
 # this will prevent TF from allocating the whole GPU
 from keras import backend as K
@@ -15,17 +16,18 @@ from keras.layers import Dense, BatchNormalization
 from keras.callbacks import ModelCheckpoint
 
 labels = ['P1', 'P2']
-categoricals = [ 'weekday', 'hour', 'node_id', 'wind_deg_bin_minus_1', 'wind_deg_bin_minus_2', 'wind_deg_bin_minus_3', 'wind_deg_bin_minus_4', 'wind_deg_bin']
+categoricals = [ 'sensor_id', 'weekday', 'hour', 'node_id', 'wind_deg_bin_minus_1', 'wind_deg_bin_minus_2', 'wind_deg_bin_minus_3', 'wind_deg_bin_minus_4', 'wind_deg_bin']
 # we might want to include day_in_year in categoricals WHEN we have at least a year of data, ideally two
 
 data = pd.read_csv('data_to_use/transformed_dust_measurement.csv')
 Y = data[labels].values
 
 data = data.drop(labels=labels, axis=1)
-data = data.drop(labels=['Unnamed: 0', 'dt_iso', 'wind_deg', 'day_in_year'], axis = 1)
+data = data.drop(labels=['Unnamed: 0', 'Unnamed: 0.1', 'dt_iso', 'wind_deg', 'day_in_year'], axis = 1)
 
 # normalization for float data
-for c in [col for col in data.columns if not col in categoricals]:
+non_categoricals = [col for col in data.columns if not col in categoricals]
+for c in non_categoricals:
     data[[c]] = scale(data[c])
 
 # here we use one-hot encoding for categorical features
@@ -50,11 +52,15 @@ cp = ModelCheckpoint('models/best_model.h5', monitor='val_mean_absolute_error', 
 
 # build and fit the multivariate multiple regressor model
 model = Sequential()
-model.add(Dense(1000, input_dim=X.shape[1], kernel_initializer='glorot_normal', activation='relu'))
+model.add(Dense(100, input_dim=X.shape[1], kernel_initializer='glorot_normal', activation='relu'))
 model.add(BatchNormalization())
-model.add(Dense(150, kernel_initializer='glorot_normal', activation='relu'))
+model.add(Dense(100, kernel_initializer='glorot_normal', activation='relu'))
 model.add(BatchNormalization())
+model.add(Dense(30, kernel_initializer='glorot_normal', activation='relu'))
 model.add(Dense(Y.shape[1], kernel_initializer='glorot_normal'))
-model.compile(loss='mean_squared_error', optimizer='adam',metrics=['mae', 'acc'])
+model.compile(loss='mean_squared_error', optimizer='adam',metrics=['mae'])
 
-model.fit(X, Y, batch_size=32,epochs=100,validation_split=0.2, callbacks=[cp])
+history = model.fit(X, Y, batch_size=32,epochs=200,validation_split=0.15, callbacks=[cp])
+
+with open('models/history.bin', 'wb') as handle: # saving the history of the model
+    pickle.dump(history.history, handle)
