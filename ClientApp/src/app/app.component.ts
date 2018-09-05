6fadeb5450c977fc73433e959ca24e6e932b83ec
Ján Neščivera <jan.nescivera@erni.sk>
2018-09-05 12:58:19 +0200

    added loading bar

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LoadingProgressService } from './service/loading-progress.service';
 import { Subscription } from 'rxjs';
+import { LoadingBarService } from '@ngx-loading-bar/core';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,12 @@ import { Subscription } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
   private requestWatcherSubscription: Subscription;
 
-  constructor(private requestService: LoadingProgressService) {}
+  constructor(
+    private requestService: LoadingProgressService,
+    private loadingBar: LoadingBarService) {}
 
   ngOnInit(): void {
-    this.requestWatcherSubscription = this.requestService.watcher.subscribe(x => console.log('some requests running;', x));
+    this.requestWatcherSubscription = this.requestService.watcher.subscribe(x => x ? this.loadingBar.start() : this.loadingBar.complete() );
   }
   ngOnDestroy(): void {
     this.requestWatcherSubscription.unsubscribe();
--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { LoadingBarModule } from '@ngx-loading-bar/core';
+
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { AboutComponent } from './about/about.component';
@@ -31,13 +33,13 @@ import { LoadingInterceptor } from './service/loading.interceptor';
       { path: '', component: OsmMapComponent, pathMatch: 'full' },
       { path: 'about', component: AboutComponent},
       { path: 'chart/:id', component: ChartComponent }
-    ])
+    ]),
+    LoadingBarModule.forRoot()
   ],
   entryComponents: [PopupInfoComponent],
   providers: [
     DustService,
-    LoadingProgressService,
-    {
+    LoadingProgressService, {
       provide: HTTP_INTERCEPTORS,
       useClass: LoadingInterceptor,
       multi: true
