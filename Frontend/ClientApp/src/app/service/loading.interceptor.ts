2733ac67b3fa0c1d32355d74e02289532da6824a
Ján Neščivera <jan.nescivera@gmail.com>
2019-03-12 23:20:48 +0100

    fixed method signature

--- Frontend/ClientApp/src/app/service/loading.interceptor.ts
+++ Frontend/ClientApp/src/app/service/loading.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { RunningRequestService } from './running-request.service';
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
@@ -9,7 +9,7 @@ export class LoadingInterceptor implements HttpInterceptor {
 
   constructor(private _requestService: RunningRequestService) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<any> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this._requestService.requestStarted();
     return next.handle(req).pipe(finalize(() => this._requestService.requestFinished()));
   }
