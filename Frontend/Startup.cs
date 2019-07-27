ecb2c46a5c72d088764940c3384bf6cafbe0011a
Ján Neščivera <jan.nescivera@gmail.com>
2019-07-27 19:45:49 +0200

    updated dependenciesto angular 8 boilerplate updated according newer dotnet angular template

--- Frontend/Startup.cs
+++ Frontend/Startup.cs
@@ -45,6 +45,7 @@ namespace Frontend
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
