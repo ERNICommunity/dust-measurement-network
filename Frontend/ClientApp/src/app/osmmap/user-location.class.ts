680c72acba33bbde2d911e9010c7e4fc176a3c66
Ján Neščivera <jan.nescivera@gmail.com>
2019-10-11 20:12:26 +0200

    improved my-location control

--- Frontend/ClientApp/src/app/osmmap/user-location.class.ts
+++ Frontend/ClientApp/src/app/osmmap/user-location.class.ts
@@ -4,8 +4,11 @@ import Map from 'ol/Map';
 export class UserLocation extends Control {
   constructor(clickCallback: (m: Map) => void) {
     const button = document.createElement('button');
+    button.type = 'button';
     button.title = 'My location';
-    button.innerHTML = '&#9678;'; // utf8 bullseye
+    const span = document.createElement('span');
+    span.innerText = '\u25ce'; // utf8 bullseye
+    button.appendChild(span);
 
     const element = document.createElement('div');
     element.className = 'user-location ol-unselectable ol-control';
