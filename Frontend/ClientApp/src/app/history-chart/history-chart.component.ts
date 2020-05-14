1412a852002d3ab90fbaec5eba1203578ad8845a
Ján Neščivera <jan.nescivera@gmail.com>
2020-05-14 17:29:24 +0200

    reactive date filter

--- Frontend/ClientApp/src/app/history-chart/history-chart.component.ts
+++ Frontend/ClientApp/src/app/history-chart/history-chart.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnInit, } from '@angular/core';
-import { DustService } from '../service/dust.service';
+
+import { BehaviorSubject, combineLatest } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
 import {  IChartistData } from 'chartist';
+
+import { DustService } from '../service/dust.service';
 
 @Component({
   selector: 'app-history-chart',
@@ -9,8 +14,8 @@ import {  IChartistData } from 'chartist';
 })
 export class HistoryChartComponent implements OnInit {
   private _id: number;
-  private _dateFrom: Date;
-  private _dateTo: Date;
+  private _dateFrom: BehaviorSubject<Date>;
+  private _dateTo: BehaviorSubject<Date>;
   data: IChartistData;
 
   @Input() set id(value: number) {
@@ -18,41 +23,31 @@ export class HistoryChartComponent implements OnInit {
   }
 
   set dateFrom(value: string) {
-    this._dateFrom = new Date(value);
-    this.updateDustData();
+    this._dateFrom .next(new Date(value));
   }
   get dateFrom(): string {
-    return this.formatForDateInput(this._dateFrom);
+    return this.formatForDateInput(this._dateFrom.value);
   }
 
   set dateTo(value: string) {
-    this._dateTo = new Date(value);
-    this.updateDustData();
+    this._dateTo.next(new Date(value));
   }
   get dateTo(): string {
-    return this.formatForDateInput(this._dateTo);
+    return this.formatForDateInput(this._dateTo.value);
   }
 
   constructor(private _dustService: DustService) {
     const now = new Date();
-    this._dateFrom = new Date(now.getFullYear(), now.getMonth(), now.getDate() - 20, 0, 0, 0, 0);
-    this._dateTo = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0 , 0, 0, 0);
+    this._dateFrom = new BehaviorSubject<Date>(new Date(now.getFullYear(), now.getMonth(), now.getDate() - 20, 0, 0, 0, 0));
+    this._dateTo = new BehaviorSubject<Date>(new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0 , 0, 0, 0));
   }
 
   ngOnInit(): void {
-    this.updateDustData();
-  }
-
-  private formatForDateInput(date: Date) {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    return `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
-  }
-
-  private updateDustData(): void {
-    this._dustService.getDustHistory(this._id, this._dateFrom, this._dateTo).subscribe(
-      results => this.data = {
+    combineLatest([this._dateFrom, this._dateTo]).pipe(
+      switchMap(([from, to]) => this._dustService.getDustHistory(this._id, from, to))
+    )
+    .subscribe(results =>
+        this.data = {
         labels: results.map(x => new Date(x.timestamp)),
         series: [
           results.map(x => x.particulateMatter25),
@@ -61,5 +56,12 @@ export class HistoryChartComponent implements OnInit {
       },
       err => console.error(err)
     );
+  }
+
+  private formatForDateInput(date: Date) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
   }
 }
